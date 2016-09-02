c145d526742da8c9ae840006b7b1e9be211428a4
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2016-09-01 22:28:18 -0500

    hseq now with custom fields

--- page-hseq.php
+++ page-hseq.php
@@ -1,8 +1,11 @@
 <?php get_header(); ?>
 
+<?php while (have_posts()) :
+the_post(); ?>
+
 <div class="quarry-header-image fixed"
-     style=" background: url(<?php echo get_template_directory_uri(); ?>/img/calidad.png) center bottom no-repeat; background-size: cover;"></div>
-<div class="title fixed">GESTIÓN INTEGRAL <b>HSEQ</b></div>
+     style=" background: url(<?php the_field('imagen_cabecera'); ?>) center bottom no-repeat; background-size: cover;"></div>
+<div class="title fixed"><?php the_title(); ?></div>
 <div id="menu-icon" class="fixed">
     <svg viewBox="0 0 24 24">
         <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/>
@@ -11,39 +14,31 @@
 <main id="panel">
     <div class="page-content-wrapper">
         <div class="padding-top"></div>
-        <div class="text">
-            REX INGENIERIA S.A., basada en su experiencia ofrece al cliente, eficacia, cumplimiento y
-            atención oportuna a sus requerimientos, dentro de las estrictas Normas Técnicas, de calidad, gestión
-            ambiental, gestión de la seguridad y la salud ocupacional que dictamina la ley.
-        </div>
 
         <div class="text">
             <img src="<?php echo get_template_directory_uri(); ?>/img/iso.png"/>
-            <div class="right">REX INGENIERÍA S.A. está certificada en los sistemas de Gestión de Calidad ISO 9001: 2008, Gestión Ambiental ISO 14001: 2004 y Gestión de la Seguridad y Salud Ocupacional OHSAS 18001: 2007, para las actividades de DISEÑO, CONSTRUCCIÓN, MANTENIMIENTO, INTERVENTORIA Y CONSULTORÍA DE OBRAS DE INFRAESTRUCTURA VIAL, URBANISMO E INFRAESTRUCTURA FLUVIAL, EXPLOTACIÓN Y COMERCIALIZACIÓN DE AGREGADOS PÉTREOS. ESTABILIZACIÓN DE TALUDES.</div>
+            <div class="right">
+                <?php the_field('primera_seccion'); ?>
+            </div>
         </div>
 
         <div class="mision-vision" id="section"
              style=" background: url(<?php echo get_template_directory_uri(); ?>/img/calidad_section.svg) center  no-repeat; background-size: cover;">
             <div class="text">
-                El Sistema de Gestión de Calidad de la Empresa REX INGENIERIA S.A. está basado en los
-                requerimientos de la norma ISO 9001:2008, con el objetivo de desarrollar los fundamentos del Sistema
-                de Gestión de Calidad, promoviendo la mejora continua del proceso, de manera que cumplan con los
-                requisitos especificados por el cliente y siga con los lineamientos especificados en la norma.
-
-                El Sistema de Gestión de Calidad garantiza la calidad de los servicios y productos prestados por la
-                empresa.
-
-                REX INGENIERÍA S.A. está certificada en los sistemas de Gestión de Calidad ISO 9001: 2008, Gestión
-                Ambiental ISO 14001: 2004 y Getión de la Seguridad y Salud Ocupacional OHSAS 18001: 2007, para las
-                actividades de DISEÑO, CONSTRUCCIÓN, MANTENIMIENTO, INTERVENTORIA Y CONSULTORÍA DE OBRAS DE
-                INFRAESTRUCTURA VIAL, URBANISMO E INFRAESTRUCTURA FLUVIAL, EXPLOTACIÓN Y COMERCIALIZACIÓN DE
-                AGREGADOS PÉTREOS. ESTABILIZACIÓN DE TALUDES.
+                <?php the_field('segunda_seccion'); ?>
             </div>
         </div>
 
         <div class="text">
-            <div class="left">REX INGENIERÍA S.A. está certificada en los sistemas de Gestión de Calidad ISO 9001: 2008, Gestión Ambiental ISO 14001: 2004 y Gestión de la Seguridad y Salud Ocupacional OHSAS 18001: 2007, para las actividades de DISEÑO, CONSTRUCCIÓN, MANTENIMIENTO, INTERVENTORIA Y CONSULTORÍA DE OBRAS DE INFRAESTRUCTURA VIAL, URBANISMO E INFRAESTRUCTURA FLUVIAL, EXPLOTACIÓN Y COMERCIALIZACIÓN DE AGREGADOS PÉTREOS. ESTABILIZACIÓN DE TALUDES.</div>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/premio.png"/>
+            <div class="left">
+                <?php the_field('tercera_seccion'); ?>
+            </div>
+            <img src="<?php the_field('imagen_premio'); ?>"/>
         </div>
+
+        <?php
+        endwhile; //resetting the page loop
+        wp_reset_query(); //resetting the page query
+        ?>
 
         <?php get_footer(); ?>
