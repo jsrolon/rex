a744f2aa580177132dec794f3541bfd70affb686
Carolina Pinzon <carolina.pinzon@lab21k.com.br>
2016-09-03 17:09:01 -0500

    fix other mistakes rolobster made

--- page-proyectos.php
+++ page-proyectos.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
+
+
 <div class="proyectos">
     <div class="title fixed">PROYECTOS</div>
 </div>
@@ -11,9 +13,20 @@
 <main id="panel" class="proyectos">
     <div class="padding-top"></div>
     <div class="page-content-wrapper center-vertical">
-
         <div class="images-circle-wrapper">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/edificacion.png" id="edificacion_img"/>
+            <div id="img1" class="imagesCircle" style="background: url(https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg) center center; background-size: cover;">
+            </div>
+            <div id="img2" class="imagesCircle" style="background: url(https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg) center center; background-size: cover;">
+            </div>
+            <div id="img3" class="imagesCircle" style="background: url(https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg) center center; background-size: cover;">
+            </div>
+            <div id="img4" class="imagesCircle" style="background: url(https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg) center center; background-size: cover;">
+            </div>
+            <div id="img5" class="imagesCircle" style="background: url(https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg) center center; background-size: cover;">
+            </div>
+            <div id="img6" class="imagesCircle" style="background: url(https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg) center center; background-size: cover;">
+            </div>
+            <!--<img src="<?php echo get_template_directory_uri(); ?>/img/edificacion.png" id="edificacion_img"/>
             <img src="<?php echo get_template_directory_uri(); ?>/img/estabilizacion.png" id="estabilizacion_img"/>
             <img src="<?php echo get_template_directory_uri(); ?>/img/movimientos.png" id="movimientos_img"/>
             <img src="<?php echo get_template_directory_uri(); ?>/img/hidraulicas.png" id="hidraulicas_img"/>
@@ -44,7 +57,44 @@
                         d="M185.497,814.058c76.881,55.857 169.472,85.942 264.503,85.942c95.031,0 187.622,-30.085 264.503,-85.942l-79.351,-109.218c-53.817,39.101 -118.631,60.16 -185.152,60.16c-66.521,0 -131.335,-21.059 -185.152,-60.16l-79.351,109.218Z"
                         id="movimientos_path"/>
                 </a>
+            </svg>-->
+            <div style="width: 400px; height: 400px;">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 100%; height: 100%;" viewbox="0 0 400 400">
+              <defs>
+                <clipPath id="svgPath1">
+                    <path d="M373.205,100c-35.726,-61.88 -101.752,-100 -173.205,-100l0,100c35.727,0 68.739,19.06 86.603,50l86.602,-50Z"/>
+                </clipPath>
+                <clipPath id="svgPath2">
+                    <path d="M373.205,300c17.554,-30.404 26.795,-64.893 26.795,-100c0,-35.107 -9.241,-69.596 -26.795,-100l-86.602,50c8.776,15.202 13.397,32.446 13.397,50c0,17.554 -4.621,34.798 -13.397,50l86.602,50Z"/>
+                </clipPath>
+                <clipPath id="svgPath3">
+                    <path d="M200,400c71.453,0 137.478,-38.12 173.205,-100l-86.602,-50c-17.864,30.94 -50.876,50 -86.603,50l0,100Z"/>
+                </clipPath>
+                <clipPath id="svgPath4">
+                    <path d="M26.795,300c35.726,61.88 101.752,100 173.205,100l0,-100c-35.727,0 -68.739,-19.06 -86.603,-50l-86.602,50Z"/>
+                </clipPath>
+                <clipPath id="svgPath5">
+                    <path d="M26.795,100c-17.554,30.404 -26.795,64.893 -26.795,100c0,35.107 9.241,69.596 26.795,100l86.602,-50c-8.776,-15.202 -13.397,-32.446 -13.397,-50c0,-17.554 4.621,-34.798 13.397,-50l-86.602,-50Z"/>
+                </clipPath>
+                <clipPath id="svgPath6">
+                    <path d="M200,0c-71.453,0 -137.479,38.12 -173.205,100l86.602,50c17.864,-30.94 50.876,-50 86.603,-50l0,-100Z" />
+                </clipPath>
+                
+              </defs>
+              <image preserveAspectRatio="xMidYMid slice" xlink:href="https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg" x="200" y="0" width="174" height="150" clip-path="url(#svgPath1)" viewbox="0 0 174 150"/>
+
+              <image preserveAspectRatio="xMidYMid slice" xlink:href="https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg" x="286.6" y="100" width="134" height="200" clip-path="url(#svgPath2)" viewbox="0 0 134 200"/>
+
+              <image preserveAspectRatio="xMidYMid slice" xlink:href="https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg" x="200" y="250" width="174" height="150" clip-path="url(#svgPath3)" viewbox="0 0 174 150"/>
+
+              <image preserveAspectRatio="xMidYMid slice" xlink:href="https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg" x="26.8" y="250" width="174" height="150" clip-path="url(#svgPath4)" viewbox="0 0 174 150"/>
+
+              <image preserveAspectRatio="xMidYMid slice" xlink:href="https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg" x="0" y="100" width="134" height="200"  clip-path="url(#svgPath5)" viewbox="0 0 134 200"/>
+
+              <image preserveAspectRatio="xMidYMid slice" xlink:href="https://image.jimcdn.com/app/cms/image/transf/dimension=1190x10000:format=jpg/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg" x="26.8" y="0" width="174" height="150" clip-path="url(#svgPath6)" viewbox="0 0 174 150"/>
+
             </svg>
+        </div>
             <div id="edificacion" class="name">
                 <a href="<?php echo get_category_link(get_cat_ID('Edificación')); ?>">EDIFICACIÓN Y
                         <div>URBANIZACIÓN</div>
@@ -71,6 +121,7 @@
                 </a>
             </div>
         </div>
+
     </div>
 
     <?php get_footer(); ?>
