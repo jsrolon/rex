a4571092ff36419f14b497036072e33f56a4b632
Carolina Pinzon <carolina.pinzon@lab21k.com.br>
2016-09-04 16:26:46 -0500

    rex cargo without gallery

--- page-empresas-asociadas.php
+++ page-empresas-asociadas.php
@@ -1,51 +1,84 @@
 <?php get_header(); ?>
 
-<div class="title fixed">EMPRESA</div>
-<div id="menu-icon" class="black fixed">
+<?php while (have_posts()) :
+the_post(); ?>
+
+<!--<div class="quarry-header-image-background fixed"></div>-->
+<div class="quarry-header-image fixed" style="width: 100%; height: 215px; overflow: hidden; position: relative;">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 1185px; height: 215px; position:absolute; transform: translateX(-50%); bottom: 0px; left: 50%;" viewbox="0 0 1185 238">
+        <defs>
+        <clipPath id="svgPath10">
+            <path d="M0,215l0,-215l1280,0l0,215l-94.944,-47.511l-146.801,47.511l-298.126,-36.751l-257.573,36.751l-89.855,-35.073l-234.519,35.073l-106.638,-38.059l-51.544,38.059Z" style="fill:none;stroke-width:1px;stroke:#000;"/>
+        </clipPath>
+        </defs>
+        <path d="M0,215l0,-215l1280,0l0,215l-94.944,-47.511l-146.801,47.511l-298.126,-36.751l-257.573,36.751l-89.855,-35.073l-234.519,35.073l-106.638,-38.059l-51.544,38.059Z" style="fill:none;stroke-width:45px;stroke:#fff;"/>
+        <rect width="1185" height="215" style="fill:rgb(255,255,255);" clip-path="url(#svgPath10)"/>
+        <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php the_field('imagen_de_cabecera'); ?>" x="0" y="0" width="1185" height="215" clip-path="url(#svgPath10)" viewbox="0 0 1185 215"/>
+        <rect width="1185" height="215" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath10)"/>
+    </svg>
+
+</div>
+<!--<div class="quarry-header-image fixed"
+     style=" background: url(<?php the_field('imagen_de_cabecera'); ?>) center bottom no-repeat; background-size: cover;">
+         <div class="dark"></div>
+     </div>-->
+
+<div class="title fixed"><?php the_title(); ?></div>
+<div id="menu-icon" class="fixed menu-icon">
     <svg viewBox="0 0 24 24">
         <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/>
     </svg>
 </div>
 <main id="panel">
-    <div class="padding-top"></div>
-    <div class="page-content-wrapper center-vertical">
+    <div class="page-content-wrapper">
+        <div class="padding-top"></div>
 
-        <div class="images-circle-wrapper">
-            <div id="name_7" class="name">
-                <a href="/empresa/quienes-somos/">QUIENES
-                        <div>SOMOS</div>
-                </a>
-            </div>
-            <div id="name_8" class="name">
-                <a href="/empresa/hseq/">GESTIÓN INTEGRAL
-                    <div>HSEQ</div>
-                </a>
-            </div>
-            
-            <div class="svgContainer">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewbox="0 0 400 400">
-                  <defs>
-                    <clipPath id="svgPath7">
-                        <path d="M200,0c-110.457,0 -200,89.543 -200,200c0,110.457 89.543,200 200,200l0,-100c-55.228,0 -100,-44.772 -100,-100c0,-55.228 44.772,-100 100,-100l0,-100Z"/>
-                    </clipPath>
-                    <clipPath id="svgPath8">
-                        <path d="M200,0c110.457,0 200,89.543 200,200c0,110.457 -89.543,200 -200,200l0,-100c55.228,0 100,-44.772 100,-100c0,-55.228 -44.772,-100 -100,-100l0,-100Z"/>
-                    </clipPath>
-                    
-                  </defs>
-
-                  <a href="/empresa/quienes-somos/">
-                  <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php the_field('imagen_quienes_somos'); ?>" x="0" y="0" width="200" height="400" clip-path="url(#svgPath7)" viewbox="0 0 200 400" class="imagesCircle" id="image_7"/>
-                  </a>
-
-                  <a href="/empresa/hseq/">
-                  <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php the_field('imagen_hseq'); ?>" x="200" y="0" width="200" height="400" clip-path="url(#svgPath8)" viewbox="0 0 200 400" class="imagesCircle" id="image_8"/>
-                  </a>
-
-
-                </svg>
+        <div class="text"><div class="subtitle"><b>REX CARGO</b></div>
+        </div>
+        <div class="section">
+            <img src="<?php the_field('logo'); ?>"/>
+            <div class="right">
+                <?php the_field('descripcion'); ?>
             </div>
         </div>
-    </div>
+        <div class="mision-vision" id="mision">
+            <div style="width: 100%; height: 335px; overflow: hidden; position: relative;">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 1185px; height: 335px; position:absolute; transform: translateX(-50%); bottom: 0px; left: 50%;" viewbox="0 0 1185 335">
+                    <defs>
+                        <clipPath id="svgPath11">
+                            <path d="M0,0l145.251,33.723l191.38,-27.515l161.624,47.48l305.062,-53.688l208.772,22.715l172.911,-22.715l0,335l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.556l-200.316,-24.325l-143.631,34.919" style="fill:none;stroke-width:1px;stroke:#000;"/>
+                        </clipPath>
+                    </defs>
+                    <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php the_field('imagen_de_mision'); ?>" x="0" y="0" width="1185" height="335" clip-path="url(#svgPath11)" viewbox="0 0 1185 335"/>
+                    <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath11)"/>
+                </svg>
+            </div>
+            <!--<div class="dark"></div>-->
+            <img src="/wp-content/themes/rex/img/mision_title.svg" />
+            <div class="text"><?php the_field('mision'); ?>
+            </div>
+        </div>
+        <div class="mision-vision" id="vision">
+            <div style="width: 100%; height: 335px; overflow: hidden; position: relative;">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 1185px; height: 335px; position:absolute; transform: translateX(-50%); bottom: 0px; left: 50%;" viewbox="0 0 1185 335">
+                    <defs>
+                        <clipPath id="svgPath12">
+                             <path d="M0,335l202.153,-30.922l132.847,29.85l139.005,-38.007l413.117,40.128l211.506,-33.563l86.372,32.514l0,-282.85l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.557l-200.316,-24.325l-143.631,34.918" style="fill:none;stroke-width:1px;stroke:#000;"/>
+                        </clipPath>
+                    </defs>
+                    <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php the_field('imagen_de_vision'); ?>" x="0" y="0" width="1185" height="335" clip-path="url(#svgPath12)" viewbox="0 0 1185 335"/>
+                    <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath12)"/>
+                </svg>
+            </div>
+            <!--<div class="dark"></div>-->
+            <img src="/wp-content/themes/rex/img/vision_title.svg" />
+            <div class="text"><?php the_field('vision'); ?></div>
+        </div>
+        <?php the_content(); ?>
 
-    <?php get_footer(); ?>
+        <?php
+        endwhile; //resetting the page loop
+        wp_reset_query(); //resetting the page query
+        ?>
+
+        <?php get_footer(); ?>
