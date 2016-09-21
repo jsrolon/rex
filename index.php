095e97719a9ab95990a8a5b1aee4ede55bf799f8
Carolina Pinzon <c.pinzon543@uniandes.edu.co>
2016-09-21 11:05:16 -0500

    Agregar botón de menu arriba a la izquierda en home

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php get_header(); ?>
+<div id="menu-icon" class="fixed">
+    <svg viewBox="0 0 24 24">
+        <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/>
+    </svg>
+</div>
 <main id="panel" class="index">
     <div class="index-carousel">
         <?php
