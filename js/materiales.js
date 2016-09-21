f0ee83380ec159382592a1661cc8c811a9f771e1
Carolina Pinzon <c.pinzon543@uniandes.edu.co>
2016-09-20 20:57:54 -0500

    add localization file

--- js/materiales.js
+++ js/materiales.js
@@ -216,39 +216,3 @@ function checkKey(e) {
     }
 
 }
-
-function es() {
-    if(location.pathname.startsWith("/test")) {
-        var pathname = location.pathname.substring(5, location.pathname.lenght);
-        if(pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/test/es' + pathname.substring(3, pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-    else {
-        if(location.pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/es' + location.pathname.substring(3, location.pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-}
-
-function en() {    
-    if(location.pathname.startsWith("/test")) {
-        var pathname = location.pathname.substring(5, location.pathname.lenght);
-        if(!pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/test/en' + pathname.substring(3, pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-    else {
-        if(!location.pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/en' + location.pathname.substring(3, location.pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-}
--- js/proyectos.js
+++ js/proyectos.js
@@ -63,39 +63,3 @@ window.onload = function() {
         window.location.href = jQuery(this).parent().attr("href");
     });
 }
-
-function es() {
-    if(location.pathname.startsWith("/test")) {
-        var pathname = location.pathname.substring(5, location.pathname.lenght);
-        if(pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/test/es' + pathname.substring(3, pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-    else {
-        if(location.pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/es' + location.pathname.substring(3, location.pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-}
-
-function en() {    
-    if(location.pathname.startsWith("/test")) {
-        var pathname = location.pathname.substring(5, location.pathname.lenght);
-        if(!pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/test/en' + pathname.substring(3, pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-    else {
-        if(!location.pathname.startsWith("/en")) {//english
-            var new_href = 'http://' + location.hostname + '/en' + location.pathname.substring(3, location.pathname.lenght);
-            console.log(new_href);
-            window.location.href = new_href;
-        }
-    }
-}
