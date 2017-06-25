345a79c715e2840d8e8fc85cca7f67cbe7c5a79a
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2017-06-25 14:27:55 -0500

    change position of text in empresas asociadas

--- page-empresas-asociadas.php
+++ page-empresas-asociadas.php
@@ -88,54 +88,56 @@ the_post(); ?>
                 <?php the_field('descripcion'); ?>
             </div>
         </div>
+
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+             style="width: 1185px; height: 335px; bottom: 0px; margin-top: 30px; left: 50%;"
+             viewbox="0 0 1185 335">
+            <defs>
+                <clipPath id="svgPath11">
+                    <path
+                            d="M0,0l145.251,33.723l191.38,-27.515l161.624,47.48l305.062,-53.688l208.772,22.715l172.911,-22.715l0,335l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.556l-200.316,-24.325l-143.631,34.919"
+                            style="fill:none;stroke-width:1px;stroke:#000;"/>
+                </clipPath>
+            </defs>
+            <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php
+            $img_id = fjarrett_get_attachment_id_by_url(get_field('imagen_de_mision'));
+            $img_src_arr = wp_get_attachment_image_src($img_id, 'full-hd');
+            $img_url = $img_src_arr[0];
+            echo $img_url;
+            ?>"
+                   x="0" y="0" width="1185" height="335" clip-path="url(#svgPath11)" viewbox="0 0 1185 335"/>
+            <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath11)"/>
+        </svg>
+
         <div class="mision-vision" id="mision">
-            <div style="width: 100%; height: 335px; overflow: hidden; position: relative;">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                     style="width: 1185px; height: 335px; position:absolute; transform: translateX(-50%); bottom: 0px; left: 50%;"
-                     viewbox="0 0 1185 335">
-                    <defs>
-                        <clipPath id="svgPath11">
-                            <path
-                                d="M0,0l145.251,33.723l191.38,-27.515l161.624,47.48l305.062,-53.688l208.772,22.715l172.911,-22.715l0,335l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.556l-200.316,-24.325l-143.631,34.919"
-                                style="fill:none;stroke-width:1px;stroke:#000;"/>
-                        </clipPath>
-                    </defs>
-                    <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php
-                    $img_id = fjarrett_get_attachment_id_by_url(get_field('imagen_de_mision'));
-                    $img_src_arr = wp_get_attachment_image_src($img_id, 'full-hd');
-                    $img_url = $img_src_arr[0];
-                    echo $img_url;
-                    ?>"
-                           x="0" y="0" width="1185" height="335" clip-path="url(#svgPath11)" viewbox="0 0 1185 335"/>
-                    <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath11)"/>
-                </svg>
-            </div>
+            <div style="width: 100%; height: 300px; overflow: hidden; position: relative;"></div>
             <!--<div class="dark"></div>-->
             <img src="<?php echo get_template_directory_uri(); ?>/img/mision_title.svg"/>
             <div class="text"><?php the_field('mision'); ?>
             </div>
         </div>
+
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+             style="width: 1185px; height: 335px; bottom: 0px; left: 50%;"
+             viewbox="0 0 1185 335">
+            <defs>
+                <clipPath id="svgPath12">
+                    <path
+                            d="M0,335l202.153,-30.922l132.847,29.85l139.005,-38.007l413.117,40.128l211.506,-33.563l86.372,32.514l0,-282.85l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.557l-200.316,-24.325l-143.631,34.918"
+                            style="fill:none;stroke-width:1px;stroke:#000;"/>
+                </clipPath>
+            </defs>
+            <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php
+            $img_id = fjarrett_get_attachment_id_by_url(get_field('imagen_de_vision'));
+            $img_src_arr = wp_get_attachment_image_src($img_id, 'full-hd');
+            $img_url = $img_src_arr[0];
+            echo $img_url;
+            ?>"
+                   x="0" y="0" width="1185" height="335" clip-path="url(#svgPath12)" viewbox="0 0 1185 335"/>
+            <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath12)"/>
+        </svg>
         <div class="mision-vision" id="vision">
-            <div style="width: 100%; height: 335px; overflow: hidden; position: relative;">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                     style="width: 1185px; height: 335px; position:absolute; transform: translateX(-50%); bottom: 0px; left: 50%;"
-                     viewbox="0 0 1185 335">
-                    <defs>
-                        <clipPath id="svgPath12">
-                            <path
-                                d="M0,335l202.153,-30.922l132.847,29.85l139.005,-38.007l413.117,40.128l211.506,-33.563l86.372,32.514l0,-282.85l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.557l-200.316,-24.325l-143.631,34.918"
-                                style="fill:none;stroke-width:1px;stroke:#000;"/>
-                        </clipPath>
-                    </defs>
-                    <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php
-                    $img_id = fjarrett_get_attachment_id_by_url(get_field('imagen_de_vision'));
-                    $img_src_arr = wp_get_attachment_image_src($img_id, 'full-hd');
-                    $img_url = $img_src_arr[0];
-                    echo $img_url;
-                    ?>"
-                           x="0" y="0" width="1185" height="335" clip-path="url(#svgPath12)" viewbox="0 0 1185 335"/>
-                    <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath12)"/>
-                </svg>
+            <div style="width: 100%; height: 300px; overflow: hidden; position: relative;">
             </div>
             <!--<div class="dark"></div>-->
             <img src="<?php echo get_template_directory_uri(); ?>/img/vision_title.svg"/>
