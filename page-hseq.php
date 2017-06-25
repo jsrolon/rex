9b6ae6ffc3fdabac65b3343de6f14f07b5abf2bf
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2017-06-25 13:55:36 -0500

    replace svg definitions in corresponding pages

--- page-hseq.php
+++ page-hseq.php
@@ -6,20 +6,24 @@ the_post(); ?>
 <!--<div class="quarry-header-image fixed"
      style=" background: url(<?php the_field('imagen_cabecera'); ?>) center bottom no-repeat; background-size: cover;"></div>-->
 <div class="quarry-header-image fixed" style="width: 100%; height: 215px; overflow: hidden; position: relative;">
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 1185px; height: 215px; position:absolute; transform: translateX(-50%); bottom: 0px; left: 50%;" viewbox="0 0 1185 238">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 2304px; height: 215px; position:absolute; transform: translateX(-50%); bottom: 0px; left: 50%;" viewBox="0 0 2304 215">
         <defs>
-        <clipPath id="svgPath10">
-            <path d="M0,215l0,-215l1280,0l0,215l-94.944,-47.511l-146.801,47.511l-298.126,-36.751l-257.573,36.751l-89.855,-35.073l-234.519,35.073l-106.638,-38.059l-51.544,38.059Z" style="fill:none;stroke-width:1px;stroke:#000;"/>
-        </clipPath>
+            <clipPath id="svgPath10" clipPathUnits="objectBoundingBox">
+                <path d="M0,1l0,-1l1,0l0,1l-0.07417,-0.2209l-0.1146,0.2209l-0.2329,-0.1709l-0.2012,0.1709l-0.07019,-0.1631l-0.1832,0.1631l-0.0833,-0.177l-0.04026,0.177Z" style="fill:none;stroke-width:1px;stroke:#000;"></path>
+            </clipPath>
         </defs>
-        <path d="M0,215l0,-215l1280,0l0,215l-94.944,-47.511l-146.801,47.511l-298.126,-36.751l-257.573,36.751l-89.855,-35.073l-234.519,35.073l-106.638,-38.059l-51.544,38.059Z" style="fill:none;stroke-width:14px;stroke:#fff;"/>
-        <rect width="1185" height="215" style="fill:rgb(255,255,255);" clip-path="url(#svgPath10)"/>
+        <svg viewBox="0 0 1280 215" preserveAspectRatio="none">
+            <path d="M0,215l0,-215l1280,0l0,215l-94.944,-47.511l-146.801,47.511l-298.126,-36.751l-257.573,36.751l-89.855,-35.073l-234.519,35.073l-106.638,-38.059l-51.544,38.059Z" style="fill:none;stroke-width:14px;stroke:#fff;"></path>
+        </svg>
+        <rect width="2304" height="215" style="fill:rgb(255,255,255);" clip-path="url(#svgPath10)"></rect>
+        <rect width="2304" height="215" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath10)"></rect>
         <image preserveAspectRatio="xMidYMid slice" xlink:href="<?php
-        $img_id = fjarrett_get_attachment_id_by_url(get_field('imagen_cabecera'));
-        $img_src_arr = wp_get_attachment_image_src( $img_id, 'full-hd' );
+        $img_id = fjarrett_get_attachment_id_by_url(get_field('imagen_de_cabecera'));
+        $img_src_arr = wp_get_attachment_image_src($img_id, 'full-hd');
         $img_url = $img_src_arr[0];
         echo $img_url;
-        ?>" x="0" y="0" width="1185" height="215" clip-path="url(#svgPath10)" viewbox="0 0 1185 215"/>
+        ?>" x="0" y="0"
+               width="1185" height="215" clip-path="url(#svgPath10)" viewbox="0 0 2304 215"/>
         <rect width="1185" height="215" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath10)"/>
     </svg>
 </div>
