d24bc75b3f990257c207633e7d2a5d55dd343c95
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2017-07-05 11:18:43 -0500

    static corrections to quienes svgs

--- js/fixSVGScaling.js
+++ js/fixSVGScaling.js
@@ -5,21 +5,23 @@
 'use strict';
 
 var recalculateSVGElementsWidths = function() {
-    var windowWidth = jQuery(window).width();
-    var headerWidth = windowWidth - jQuery('nav#menu').width();
-    var viewboxString = "0 0 " + headerWidth + " 215";
-    console.log(viewboxString);
+    if(jQuery(window).width() > 768) { // only apply corrections for desktop sizes
+        var windowWidth = jQuery(window).width();
+        var headerWidth = windowWidth - jQuery('nav#menu').width();
+        var viewboxString = "0 0 " + headerWidth + " 215";
+        console.log(viewboxString);
 
-    var svg = document.getElementById('outer-svg-element');
-    svg.style.width = headerWidth;
-    svg.setAttribute('viewBox', viewboxString);
+        var svg = document.getElementById('outer-svg-element');
+        svg.style.width = headerWidth;
+        svg.setAttribute('viewBox', viewboxString);
 
-    var jQuerySVG = jQuery(svg);
-    jQuerySVG.children('rect').attr('width', headerWidth);
+        var jQuerySVG = jQuery(svg);
+        jQuerySVG.children('rect').attr('width', headerWidth);
 
-    var image = document.getElementById('inner-svg-image');
-    image.setAttribute('width', headerWidth);
-    image.setAttribute('viewBox', viewboxString);
+        var image = document.getElementById('inner-svg-image');
+        image.setAttribute('width', headerWidth);
+        image.setAttribute('viewBox', viewboxString);
+    }
 };
 
 recalculateSVGElementsWidths();
--- page-quienes-somos.php
+++ page-quienes-somos.php
@@ -50,11 +50,11 @@ the_post(); ?>
         <div style="margin-top: 45px; margin-bottom: 30px;" class="text"><?php the_field('descripcion'); ?></div>
 
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-             style="width: 1185px; height: 335px; position:relative; transform: translateX(-50%); bottom: 0px; left: 50%;"
+             style="width: 1185px; height: 335px; display: block;"
              viewbox="0 0 1185 335">
             <defs>
-                <clipPath id="svgPath11">
-                    <path d="M0,0l145.251,33.723l191.38,-27.515l161.624,47.48l305.062,-53.688l208.772,22.715l172.911,-22.715l0,335l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.556l-200.316,-24.325l-143.631,34.919"
+                <clipPath id="svgPath11" clipPathUnits="objectBoundingBox">
+                    <path d="M0,0l0.11347734375,0.10066567164179104l0.14951562499999999,-0.08213432835820896l0.12626874999999999,0.14173134328358208l0.2383296875,-0.1602626865671642l0.163103125,0.06780597014925373l0.13508671875,-0.06780597014925373l0,1l-0.135171875,-0.11322985074626866l-0.18832421875,0.11322985074626866l-0.1571203125,-0.15567164179104478l-0.17645625,0.12404776119402984l-0.156496875,-0.07261194029850745l-0.11221171875,0.10423582089552239"
                           style="fill:none;stroke-width:1px;stroke:#000;"/>
                 </clipPath>
             </defs>
@@ -63,8 +63,8 @@ the_post(); ?>
             $img_src_arr = wp_get_attachment_image_src($img_id, 'full-hd');
             $img_url = $img_src_arr[0];
             echo $img_url;
-            ?>" x="0" y="0" width="1185" height="335" clip-path="url(#svgPath11)" viewbox="0 0 1185 335"/>
-            <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath11)"/>
+            ?>" x="0" y="0" width="1280" height="335" clip-path="url(#svgPath11)" viewbox="0 0 1280 335"/>
+            <rect width="1280" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath11)"/>
         </svg>
         <div class="mision-vision" id="mision">
             <div style="width: 100%; height: 300px; overflow: hidden; position: relative;">
@@ -76,11 +76,11 @@ the_post(); ?>
         </div>
 
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-             style="width: 1185px; height: 335px; position:relative; transform: translateX(-50%); bottom: 0px; left: 50%;"
+             style="width: 1185px; height: 335px; display: block;"
              viewbox="0 0 1185 335">
             <defs>
-                <clipPath id="svgPath12">
-                    <path d="M0,335l202.153,-30.922l132.847,29.85l139.005,-38.007l413.117,40.128l211.506,-33.563l86.372,32.514l0,-282.85l-173.02,-37.932l-241.055,37.932l-201.114,-52.15l-225.864,41.557l-200.316,-24.325l-143.631,34.918"
+                <clipPath id="svgPath12" clipPathUnits="objectBoundingBox">
+                    <path d="M0,0l0.11347734375,0.10066567164179104l0.14951562499999999,-0.08213432835820896l0.12626874999999999,0.14173134328358208l0.2383296875,-0.1602626865671642l0.163103125,0.06780597014925373l0.13508671875,-0.06780597014925373l0,1l-0.135171875,-0.11322985074626866l-0.18832421875,0.11322985074626866l-0.1571203125,-0.15567164179104478l-0.17645625,0.12404776119402984l-0.156496875,-0.07261194029850745l-0.11221171875,0.10423582089552239"
                           style="fill:none;stroke-width:1px;stroke:#000;"/>
                 </clipPath>
             </defs>
@@ -89,8 +89,8 @@ the_post(); ?>
             $img_src_arr = wp_get_attachment_image_src($img_id, 'full-hd');
             $img_url = $img_src_arr[0];
             echo $img_url;
-            ?>" x="0" y="0" width="1185" height="335" clip-path="url(#svgPath12)" viewbox="0 0 1185 335"/>
-            <rect width="1185" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath12)"/>
+            ?>" x="0" y="0" width="1280" height="335" clip-path="url(#svgPath12)" viewbox="0 0 1280 335"/>
+            <rect width="1280" height="335" style="fill:rgba(0,0,0,0.2);" clip-path="url(#svgPath12)"/>
         </svg>
         <div class="mision-vision" id="vision">
             <div style="width: 100%; height: 300px; overflow: hidden; position: relative;">
